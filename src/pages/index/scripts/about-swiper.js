f863af8d758af09131b20d6aee1a9c1cd781a675
KseniyaYaskevich <yaskevi4kseniya@gmail.com>
2022-12-09 12:59:03 +0300

    feat: add swiper for about section

---
+++ src/pages/index/scripts/about-swiper.js
@@ -0,0 +1,25 @@
+import Swiper, {Navigation, Pagination} from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+const swiperAboutCompany = new Swiper('.about-company__swiper', {
+  direction: 'horizontal',
+  modules: [Navigation, Pagination],
+  navigation: {
+    nextEl: '.about-company__button-next',
+    prevEl: '.about-company__button-prev',
+  },
+});
+
+const swiperAboutProducts = new Swiper('.about-products__swiper', {
+  direction: 'horizontal',
+  modules: [Navigation, Pagination],
+  navigation: {
+    nextEl: '.about-products__button-next',
+    prevEl: '.about-products__button-prev',
+  },
+});
+
+swiperAboutCompany.update();
+swiperAboutProducts.update();
--- src/pages/index/scripts/script.js
+++ src/pages/index/scripts/script.js
@@ -1,3 +1,4 @@
 import './main-nav-toggle';
 import './discounts-swiper';
 import './sport-goods-swiper';
+import './about-swiper';
